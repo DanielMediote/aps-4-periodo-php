ce2c87cd25ac603fdbff247dd0ebc94cefabfbdb
Daniel Mediote <daniel_mediote@hotmail.com>
2018-08-22 21:47:56 -0400

    .

---
+++ src/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="pt" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>APS</title>
+  </head>
+  <body>
+    <h1>Olá Mundo! Aqui será o inicio da APS.</h1>
+  </body>
+</html>
